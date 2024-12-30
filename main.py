1289eed76889d6d0e6634d41d7dac7bbc00e25a0
yousefhaitham2 <youssef.haitham@meti.dev>
2024-12-30 09:42:21 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+start_year = int(input("Enter your birth year: "))  
+end_year = int(input("Enter the current year: "))  
+
+# Calculate age
+age = end_year - start_year
+
+# Print the calculated age
+print("Your age is:", age)
